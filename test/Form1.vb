431b85c704054f69eb5c6cfe7e623b9dacf17f33
Alexander Baggett <alexander.baggett@gmail.com>
2017-02-28 14:44:26 -0600

    Built example

---
+++ test/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Public Sub New()
+        Dim text As String = System.IO.File.ReadAllText("svg.svg")
+        InitializeComponent()
+        WebBrowser1.DocumentText = text
+    End Sub
+
+End Class
